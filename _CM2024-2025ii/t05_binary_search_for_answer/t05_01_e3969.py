1002c775d5e6a7f4a8693cdf29dcc8a4a85740f4
Arsen <avklevtsovskiy@gmail.com>
2025-02-03 13:34:11 +0200

    T05

---
+++ _CM2024-2025ii/t05_binary_search_for_answer/t05_01_e3969.py
@@ -0,0 +1,16 @@
+def bsearch(w, h, n):
+    l = 0
+    r = n * max(w, h)
+    while l < r:
+        m = l + (r - l) // 2
+        count = (m // w) * (m // h)
+        if count < n:
+            l = m + 1
+        else:
+            r = m
+    return l
+
+
+if __name__ == '__main__':
+    w, h, n = map(int, input().split())
+    print(bsearch(w, h, n))
