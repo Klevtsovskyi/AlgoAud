65ba2c0054598942193b21b637eba2b508930fdc
Arsen <avklevtsovskiy@gmail.com>
2025-03-17 12:06:15 +0200

    T12

---
+++ _CM2024-2025ii/t12_stack/t09_02/user.py
@@ -0,0 +1,48 @@
+
+"""
+Реалізуйте швидкий алгоритм сортування QuickSort.
+"""
+
+N = 1000000  # Кількість елементів масиву.
+             # Використовується у головній програмі для генерування масиву з випадкових чисел
+             # Для повільних алгоритмів сортування з асимптотикою n**2 рекомендується
+             # використовувати значення не більше 10к
+             # Для швидких алгоритмів сортування з асимптотикою
+             # nlog(n) встановіть значення 1 000 000
+
+
+def sort(array):
+    """ Сортування масиву
+    :param array: Вхідний масив даних, що треба відсортувати.
+    """
+    stack = []
+    stack.append((0, len(array) - 1))
+    while stack:
+        a, b = stack.pop()
+        if a >= b:
+            continue
+        pivot = array[a + (b - a + 1) // 2]
+        left = a
+        right = b
+        while True:
+            while array[left] < pivot:
+                left += 1
+            while array[right] > pivot:
+                right -= 1
+
+            if left >= right:
+                break
+
+            array[left], array[right] = array[right], array[left]
+            left += 1
+            right -= 1
+
+        stack.append((left, b))
+        stack.append((a, left - 1))
+
+
+if __name__ == '__main__':
+    arr = [9, 1, -1, 20, 19, 10, -5, 13, 7]
+    print(arr)
+    sort(arr)
+    print(arr)
---
+++ _CM2024-2025ii/t12_stack/t12_02_e6124.py
@@ -0,0 +1,59 @@
+class Node:
+
+    def __init__(self, item):
+        self.item = item
+        self.next = None
+
+
+class Stack:
+
+    def __init__(self):
+        self._top = None
+        self._size = 0
+
+    def empty(self):
+        return self._top is None
+
+    def push(self, item):
+        node = Node(item)
+        node.next = self._top
+        self._top = node
+        self._size += 1
+        return "ok"
+
+    def pop(self):
+        if self.empty():
+            return "error"
+        item = self._top.item
+        self._top = self._top.next
+        self._size -= 1
+        return item
+
+    def back(self):
+        if self.empty():
+            return "error"
+        return self._top.item
+
+    def size(self):
+        return self._size
+
+    def clear(self):
+        self.__init__()
+        return "ok"
+
+    def exit(self):
+        return "bye"
+
+    def execute(self, command):
+        method, *args = command.split()
+        return getattr(self, method)(*args)
+
+
+if __name__ == '__main__':
+    with open("input.txt") as f:
+        stack = Stack()
+        for line in f:
+            res = stack.execute(line)
+            print(res)
+            if res == "bye":
+                break
