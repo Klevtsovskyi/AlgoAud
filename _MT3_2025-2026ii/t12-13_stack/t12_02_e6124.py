cae6d921bfaae3df8e0f55da0f7a0bf1b6af9a2e
Arsen Klevtsovskyi <avklevtsovskiy@gmail.com>
2026-04-15 11:16:34 +0300

    M3 T12

--- _MT3_2025-2026ii/t12-13_stack/t12_02_e6124.py
+++ _MT3_2025-2026ii/t12-13_stack/t12_02_e6124.py
@@ -1,24 +1,44 @@
+class Node:
+
+    def __init__(self, item):
+        self.item = item
+        self.next: Node | None = None
+
+
 class Stack:
 
     def __init__(self):
-        ...
+        self._top: Node | None = None
+        self._size = 0
 
     def empty(self):
-        return True
+        return self._top is None
 
-    def push(self, n):
+    def push(self, item):
+        node = Node(item)
+        node.next = self._top
+        self._top = node
+        self._size += 1
         return "ok"
 
     def pop(self):
-        return 0
+        if self.empty():
+            return "error"
+        item = self._top.item
+        self._top = self._top.next
+        self._size -= 1
+        return item
 
     def back(self):
-        return 0
+        if self.empty():
+            return "error"
+        return self._top.item
 
     def size(self):
-        return 0
+        return self._size
 
     def clear(self):
+        self.__init__()
         return "ok"
 
     def exit(self):
